f035a4cf6088963280c5b564fcfdc1f771f9c959
Humberto Dantas <humbertoodantas@gmail.com>
2020-11-23 14:03:14 -0300

    Calculates averages prices - extends Forma

---
+++ src/main/java/br/com/sec4you/MediaPrecos.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+public class MediaPrecos extends Forma {
+    private float totalPrecos;
+    private int mediaPrecos;
+
+    public MediaPrecos() {
+    }
+
+    public MediaPrecos(float totalPrecos, int mediaPrecos) {
+        this.totalPrecos = totalPrecos;
+        this.mediaPrecos = mediaPrecos;
+    }
+
+    public float getTotalPrecos() {
+        return totalPrecos;
+    }
+    public int getMediaPrecos() {
+        return mediaPrecos;
+    }
+
+    public void setTotalPrecos(float totalPrecos) {
+        this.totalPrecos = totalPrecos;
+    }
+    public void setMediaPrecos(int mediaPrecos) {
+        this.mediaPrecos = mediaPrecos;
+    }
+
+    @Override
+    public float calculaTotal(){
+        return totalPrecos/mediaPrecos;
+    }
+    public void mostra(){
+        JOptionPane.showMessageDialog(null, "Média de preços para o modelo digitado: \nR$" + calculaTotal());
+    }
+}
