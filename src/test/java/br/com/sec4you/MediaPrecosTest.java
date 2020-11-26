38526c5489f9c95c6bcbc90a8d3c41943594c682
Humberto Dantas <humbertoodantas@gmail.com>
2020-11-26 17:03:17 -0300

    MediaPrecos.java Test

--- src/test/java/br/com/sec4you/MediaPrecosTest.java
+++ src/test/java/br/com/sec4you/MediaPrecosTest.java
@@ -3,5 +3,9 @@ package br.com.sec4you;
 import junit.framework.TestCase;
 
 public class MediaPrecosTest extends TestCase {
-	
+	public void testMediPrecos() {
+		MediaPrecos s = new MediaPrecos(149.9f, 8);
+		
+		assertTrue(s.calculaTotal() == 18.7375f);
+	}
 }
