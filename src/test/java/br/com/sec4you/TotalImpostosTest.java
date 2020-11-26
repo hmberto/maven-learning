00f1fcd7bb0f85f345c418deaddf16a926a4603f
Humberto Dantas <humbertoodantas@gmail.com>
2020-11-26 17:02:14 -0300

    JOptionPane removed

--- src/test/java/br/com/sec4you/TotalImpostosTest.java
+++ src/test/java/br/com/sec4you/TotalImpostosTest.java
@@ -1,16 +1,11 @@
 package br.com.sec4you;
 
-import javax.swing.JOptionPane;
-
 import junit.framework.TestCase;
 
 public class TotalImpostosTest extends TestCase {
 	public void testCalculaTotal() {
 		TotalImpostos v = new TotalImpostos(15, 9.9f);
-        v.calculaTotal();
-        
-        float expectedResult = 148.5f;
-        
-		assertTrue(v.calculaTotal() == expectedResult);
+                
+		assertTrue(v.calculaTotal() == 148.5f);
 	}
 }
