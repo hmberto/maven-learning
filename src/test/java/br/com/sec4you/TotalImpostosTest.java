2af5bc5cb0886f45870772c4fbadcbdc0e29d1c3
Humberto Dantas <humbertoodantas@gmail.com>
2020-11-26 16:41:19 -0300

    TotalImpostos.java Test

--- src/test/java/br/com/sec4you/TotalImpostosTest.java
+++ src/test/java/br/com/sec4you/TotalImpostosTest.java
@@ -5,5 +5,12 @@ import javax.swing.JOptionPane;
 import junit.framework.TestCase;
 
 public class TotalImpostosTest extends TestCase {
-	
+	public void testCalculaTotal() {
+		TotalImpostos v = new TotalImpostos(15, 9.9f);
+        v.calculaTotal();
+        
+        float expectedResult = 148.5f;
+        
+		assertTrue(v.calculaTotal() == expectedResult);
+	}
 }
